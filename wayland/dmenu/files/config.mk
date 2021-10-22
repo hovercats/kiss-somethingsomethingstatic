9a09856b3534d29a97d45cc96fe2d58cea582678
hovercats <hovercatswithlasereyes@protonmail.com>
2021-10-22 23:20:15 +0200

    dmenu: static. use modified config.mk file. add depends. closes #8

---
+++ wayland/dmenu/files/config.mk
@@ -0,0 +1,21 @@
+# dmenu version
+VERSION = 4.6
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+PIXMANINC = /usr/include/pixman-1
+SWCPROTO = /usr/share/swc/swc.xml
+
+# includes and libs
+INCS = -I${PIXMANINC}
+LIBS = -lwayland-client -lxkbcommon -lwld -lfontconfig -lffi -lpixman-1 -lfreetype -lpng -lxml2 -lz -lwld -ldrm -lexpat
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\"
+CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  = -s ${LIBS} -static
+
+# compiler and linker
+CC = cc
